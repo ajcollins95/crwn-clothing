371b4391497c9367ed95669ebc7cd83a813e1201
Aaron Collins <aa.collins.95@gmail.com>
2020-07-14 11:05:18 -0400

    Created sign in component and button with styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route  path='/shop' component={ShopPage} />
+        <Route  path='/signin' component={SignInAndSignUpPage} />
+
       </Switch>
     </div>
   );
