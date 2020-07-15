33962ea6e183aa8ce46be0034747a856df2a6c6c
Aaron Collins <aa.collins.95@gmail.com>
2020-07-15 14:18:46 -0400

    Every change made through video 15

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+//import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -24,10 +24,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state)
+        });   
+      }
+      
+      this.setState({currentUser: userAuth})
+      
+
+
     });
   }
 
@@ -41,8 +57,8 @@ class App extends React.Component {
         <Header currentUser={this.state.curentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route  path='/shop' component={ShopPage} />
-          <Route  path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
   
         </Switch>
       </div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,44 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBQi9zTRaQGuOfIGHX3xCAuU6sMjUQ2sCg",
-    authDomain: "crwn-db-c5c37.firebaseapp.com",
-    databaseURL: "https://crwn-db-c5c37.firebaseio.com",
-    projectId: "crwn-db-c5c37",
-    storageBucket: "crwn-db-c5c37.appspot.com",
-    messagingSenderId: "150954350247",
-    appId: "1:150954350247:web:e2014045128aff651a2d82"
-  };
+  apiKey: "AIzaSyBQi9zTRaQGuOfIGHX3xCAuU6sMjUQ2sCg",
+  authDomain: "crwn-db-c5c37.firebaseapp.com",
+  databaseURL: "https://crwn-db-c5c37.firebaseio.com",
+  projectId: "crwn-db-c5c37",
+  storageBucket: "crwn-db-c5c37.appspot.com",
+  messagingSenderId: "150954350247",
+  appId: "1:150954350247:web:e2014045128aff651a2d82"
+};
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef;
+}
+
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
